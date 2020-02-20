3787e56f9c6908dea010f6d16addf43549e12fb6
johnpap84 <48949781+johnpap84@users.noreply.github.com>
2020-02-20 10:38:12 +0000

    Add files via upload

--- Employees/employees-menu/employees-menu.component.ts
+++ Employees/employees-menu/employees-menu.component.ts
@@ -1,69 +1,106 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { trigger, transition, animate, style } from '@angular/animations'
 import { EmployeeServices } from '../../Services/employeeServices';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { Department } from 'src/app/Models/department';
 import { JobTitle } from 'src/app/Models/jobTitle';
+import { Employee } from '../../Models/employee';
 
 @Component({
   selector: 'empmenu',
   templateUrl: './employees-menu.component.html',
-  styleUrls: ['../../app.component.css', 'employees-menu.component.css']
+  styleUrls: ['../../app.component.css', 'employees-menu.component.css'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({ transform: 'translateY(-20%)' }),
+        animate('500ms ease-in', style({ transform: 'translateY(0%)', opacity: '100' }))
+      ]),
+      transition(':leave', [
+        animate('500ms ease-in', style({ transform: 'translateX(-100%)', opacity: '0' }))
+      ])
+    ])
+  ]
 })
 
 export class EmpMenuBar implements OnInit {
 
-    constructor(private employeeData: EmployeeServices) {}
+  constructor(private employeeData: EmployeeServices) { }
 
-    @Input() departments: Department[];
-    private roles: string[] = [];
-    private newEmployeeForm: FormGroup;
-    private validMessage: string = "";
-    private showNewEmployee = false;
+  @Input() departments: Department[];
+  @Input() employees: Employee[];
+  private jobTitles: JobTitle[] = [];
 
-    private jobTitles: JobTitle[] = [];
-    departmentSelection(dep: string) {
+  private newEmployeeForm: FormGroup;
+  private validMessage: string = "";
+  private showNewEmployee: boolean = false;
+  private showEmployeesList: boolean = false;
 
-      for(let d of this.departments) {
-        if (d.departmentName == dep) {
-          this.jobTitles = d.jobTitles;
-        }
+  private _departmentFilter: any = { departmentId: '' };
+
+  departmentSelection(dep: string) {
+    for (let d of this.departments) {
+      if (d.departmentName == dep) {
+        this.jobTitles = d.jobTitles;
       }
     }
+  }
 
-    ngOnInit() {
-      this.newEmployeeForm = new FormGroup({
-        name: new FormControl('', Validators.required),
-        email: new FormControl('', Validators.required),
-        
-        phoneNumber: new FormControl('', Validators.required),
-        department: new FormControl('', Validators.required),
-        jobTitle: new FormControl('', Validators.required),
+  ngOnInit() {
+    this.newEmployeeForm = new FormGroup({
 
-        holidayQuota: new FormControl('20', Validators.required),
+      name: new FormControl('', Validators.required),
+      email: new FormControl(''),
+      phoneNumber: new FormControl(''),
+      department: new FormControl('', Validators.required),
+      jobTitle: new FormControl('', Validators.required),
+      holidayQuota: new FormControl(20, Validators.required),
 
-        contact: new FormControl()
-      });
+      contact: new FormControl()
+    });
+  }
+
+  toggleShowNewEmployee() {
+
+    if (this.showEmployeesList == true) {
+      this.showEmployeesList = false;
+      setTimeout(() => {
+        this.showNewEmployee = !this.showNewEmployee;
+      }, 400);
+    } else {
+      this.showNewEmployee = !this.showNewEmployee;
     }
+  }
 
-    toggleShowNewEmployee() { this.showNewEmployee = !this.showNewEmployee;}
+  toggleEmployeesList() {
 
-    newEmployee() {
-      console.log(this.newEmployeeForm.value);
-      if (this.newEmployeeForm.valid) {
-        this.validMessage = "New employee has been added to the Database.";
-        
-        this.employeeData.addEmployee(this.newEmployeeForm.value)
-          .subscribe(data => {
-            this.newEmployeeForm.reset();
-            location.reload();
-            return true;
-          },
+    if (this.showNewEmployee == true) {
+      this.showNewEmployee = false;
+      setTimeout(() => {
+        this.showEmployeesList = !this.showEmployeesList;
+      }, 400);
+    } else {
+      this.showEmployeesList = !this.showEmployeesList;
+    }
+  }
+
+  newEmployee() {
+    console.log(this.newEmployeeForm.valid);
+    if (this.newEmployeeForm.valid) {
+      this.validMessage = "New employee has been added to the Database.";
+
+      this.employeeData.addEmployee(this.newEmployeeForm.value)
+        .subscribe(data => {
+          this.newEmployeeForm.reset();
+          location.reload();
+          return true;
+        },
           error => { return Observable.throw(error); }
-          )
-      } else {
-        this.validMessage = "Failed to add new employee to the Database.";
-      }
+        )
+    } else {
+      this.validMessage = "Failed to add new employee to the Database.";
     }
+  }
 
 }
