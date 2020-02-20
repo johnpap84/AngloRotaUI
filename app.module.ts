18d17e9208e506237bf009803834947aab3b5693
johnpap84 <48949781+johnpap84@users.noreply.github.com>
2020-02-20 10:43:24 +0000

    Add files via upload

--- app.module.ts
+++ app.module.ts
@@ -6,10 +6,12 @@ import { routing } from './app.routing';
 import { FilterPipeModule } from 'ngx-filter-pipe';
 import { AutofocusDirective } from './Rota/autofocus.directive';
 import { EditInputComponent } from './Rota/edit-input/edit-input.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { EmployeeServices } from './Services/employeeServices';
 import { DepartmentServices } from './Services/departmentServices'; 
 import { JobTitleServices } from './Services/jobTitleServices';
+import { ShiftServices } from './Services/shiftServices';
 import { AuthServices } from './Auth/authServices';
 
 //// MY COMPONENTS ////
@@ -20,18 +22,21 @@ import { HomeComponent } from './Home/home.component';
 import { EmployeesListComponent } from './Employees/employees.component';
 import { EmpMenuBar } from './Employees/employees-menu/employees-menu.component';
 import { RotaModule } from './Rota/rota.component';
+import { RotaMenu } from './Rota/rota-menu/rota-menu.component';
 import { DepartmentsComponent } from './Departments/departments.component';
 import { DepartmentsMenu } from './Departments/departments-menu/departments-menu.component';
 import { JobTitlesComponent } from './JobTitles/jobTitles.component';
 import { JobTitlesMenu } from './JobTitles/jobTitles-menu/jobTitles-menu.component';
+import { ShiftComponent } from './Shift/shift.component';
+import { ShiftMenu } from './Shift/shift-menu/shift-menu.component';
 
 @NgModule({
   declarations: [ AppComponent, EditInputComponent, AutofocusDirective, Login, MenuBar, HomeComponent, EmployeesListComponent, EmpMenuBar, 
-                  DepartmentsComponent, DepartmentsMenu, JobTitlesComponent, JobTitlesMenu, RotaModule ],
+    DepartmentsComponent, DepartmentsMenu, JobTitlesComponent, JobTitlesMenu, ShiftComponent, ShiftMenu, RotaModule, RotaMenu ],
 
-  imports: [ BrowserModule, HttpClientModule, FormsModule, ReactiveFormsModule, FilterPipeModule, routing ],
+  imports: [BrowserModule, HttpClientModule, FormsModule, ReactiveFormsModule, FilterPipeModule, routing, BrowserAnimationsModule ],
 
-  providers: [ EmployeeServices, DepartmentServices, JobTitleServices, AuthServices ],
+  providers: [EmployeeServices, DepartmentServices, JobTitleServices, ShiftServices, AuthServices ],
 
   bootstrap: [AppComponent]
 })
--- app.routing.ts
+++ app.routing.ts
@@ -1,5 +1,5 @@
 import { Routes, RouterModule } from '@angular/router';
-import { AuthGuard} from './Auth/auth.guard';
+import { AuthGuard } from './Auth/auth.guard';
 
 import { Login } from './LogIn/login.component';
 import { HomeComponent } from './Home/home.component';
@@ -7,20 +7,23 @@ import { EmployeesListComponent } from './Employees/employees.component';
 import { EmpMenuBar } from './Employees/employees-menu/employees-menu.component';
 import { RotaModule } from './Rota/rota.component';
 import { DepartmentsComponent } from './Departments/departments.component';
+import { ShiftComponent } from './Shift/shift.component';
 import { JobTitlesComponent } from './JobTitles/jobTitles.component';
 
 const routes: Routes = [
-    
-    { path: '', component: Login },
-    { path: 'Home', component: HomeComponent, canActivate: [AuthGuard] },
-    { path: 'Employees', component: EmployeesListComponent, canActivate: [AuthGuard] },
-    { path: 'Departments', component: DepartmentsComponent, canActivate: [AuthGuard] },
-    { path: 'JobTitles', component: JobTitlesComponent, canActivate: [AuthGuard] },
-    { path: 'EmployeesMenu', component: EmpMenuBar, canActivate: [AuthGuard] },
-    { path: 'RotaModule', component: RotaModule, canActivate: [AuthGuard] }
-  ];
+
+  { path: '', component: Login },
+  { path: 'Home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'Employees', component: EmployeesListComponent, canActivate: [AuthGuard] },
+  { path: 'Departments', component: DepartmentsComponent, canActivate: [AuthGuard] },
+  { path: 'JobTitles', component: JobTitlesComponent, canActivate: [AuthGuard] },
+  { path: 'EmployeesMenu', component: EmpMenuBar, canActivate: [AuthGuard] },
+  { path: 'RotaModule', component: RotaModule, canActivate: [AuthGuard] },
+  { path: 'Shifts', component: ShiftComponent, canActivate: [AuthGuard] }
+
+];
 
 export const routing = RouterModule.forRoot(routes, {
-    useHash: false,
-    enableTracing: true
+  useHash: false,
+  enableTracing: false
 });
