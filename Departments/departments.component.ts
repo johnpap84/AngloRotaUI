06f58f394a315c5b14c99c6476b6d3b2056e8e9a
johnpap84 <48949781+johnpap84@users.noreply.github.com>
2020-02-20 10:36:18 +0000

    Add files via upload

--- Departments/departments.component.ts
+++ Departments/departments.component.ts
@@ -5,44 +5,48 @@ import { Department } from '../Models/department';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-    templateUrl: 'departments.component.html',
-    providers: [DepartmentServices],
-    styleUrls: ['../app.component.css', 'departments.component.css']
-  })
+  templateUrl: 'departments.component.html',
+  providers: [DepartmentServices],
+  styleUrls: ['../app.component.css', 'departments.component.css']
+})
 
 export class DepartmentsComponent implements OnInit {
 
-    constructor (private departmentData: DepartmentServices, private filterPipe: FilterPipe) {}
+  constructor(private departmentData: DepartmentServices, private filterPipe: FilterPipe) { }
 
-    public departments: Department[] = [];
-    editDepartmentForm: FormGroup;
-    showEditDepartment: boolean = false;
-    editThisDepartment: number = 0;
+  public departments: Department[] = [];
+  editDepartmentForm: FormGroup;
+  showEditDepartment: boolean = false;
+  editThisDepartment: number = 0;
 
-    toggleEditDepartment(id: number) { this.showEditDepartment = !this.showEditDepartment; this.editThisDepartment = id; }
+  toggleEditDepartment(id: number) { this.showEditDepartment = !this.showEditDepartment; this.editThisDepartment = id; }
 
-    ngOnInit(): void {
-        this.departmentData.loadAllDepartments()
-            .subscribe(success => {
-              if (success) {
-                this.departments = this.departmentData.departments;
-                console.log(this.departments);
-              }
-            });
-
-          this.editDepartmentForm = new FormGroup({
-      
-              departmentId: new FormControl('', Validators.required),
-              departmentName: new FormControl('', Validators.required)
-            }); 
+  ngOnInit(): void {
+    this.departmentData.loadAllDepartments()
+      .subscribe(success => {
+        if (success) {
+          this.departments = this.departmentData.departments;
         }
+      });
 
-        editDepartment(id: number) {
+    this.editDepartmentForm = new FormGroup({
 
-          this.editDepartmentForm.get('departmentId').setValue(id);
-          
-          this.departmentData.updateDepartment(this.editDepartmentForm.value);
-          location.reload();
-        }
+      departmentId: new FormControl('', Validators.required),
+      departmentName: new FormControl('', Validators.required)
+    });
+  }
+
+  editDepartment(id: number) {
+
+    this.editDepartmentForm.get('departmentId').setValue(id);
+
+    this.departmentData.updateDepartment(this.editDepartmentForm.value);
+    location.reload();
+  }
+
+  deleteDepartment(id: number): void {
+    this.departmentData.deleteDepartment(id);
+    location.reload();
+  }
 
 }
