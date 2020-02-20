932ec6d814bb821074c2207316aec79a9e0477d1
johnpap84 <48949781+johnpap84@users.noreply.github.com>
2020-02-20 10:39:28 +0000

    Add files via upload

--- MenuBar/menu.bar.component.ts
+++ MenuBar/menu.bar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthServices } from '../Auth/authServices';
 
+
 @Component({
   selector: 'menubar',
   templateUrl: './menu.bar.component.html',
@@ -13,11 +14,30 @@ export class MenuBar {
   constructor(private router: Router, private authServices: AuthServices) { }
 
   private isLoggedIn: boolean;
+  private isToolsMenuVisible: boolean = false;
+  private isToolsMenuIn: boolean = false;
 
   logout() {
     this.authServices.logout();
     this.isLoggedIn = false;
     this.router.navigate(['']);
   }
+
+  showToolsMenu() {
+    this.isToolsMenuVisible = true;
+  }
+
+  hideToolsMenu() {
+    setTimeout(() => this.isToolsMenuVisible = false, 1000)
+  }
+
+  showToolsMenuIn() {
+    this.isToolsMenuIn = true;
+  }
+
+  hideToolsMenuOut() {
+    setTimeout(() => this.isToolsMenuIn = false, 1000)
+  }
+  
 
 }
